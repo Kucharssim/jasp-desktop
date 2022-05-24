87471a8347bbac3a2e248d5468de72d9ce04dede
Kucharssim <kucharssim@gmail.com>
2022-05-24 12:57:41 +0200

    add jaspLearnStats to Modules.cmake

--- Tools/CMake/Modules.cmake
+++ Tools/CMake/Modules.cmake
@@ -37,6 +37,7 @@ set(JASP_EXTRA_MODULES
     "jaspEquivalenceTTests"
     "jaspJags"
     "jaspLearnBayes"
+    "jaspLearnStats"
     "jaspMachineLearning"
     "jaspMetaAnalysis"
     "jaspNetwork"
